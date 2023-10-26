26ba3658e815a13cf57533ee86dc9a1e87f02c6a
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-10-27 01:11:35 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+await waitFor(() => {
+  expect(screen.getByText('please pass this test')).toBeInTheDocument();
+});
